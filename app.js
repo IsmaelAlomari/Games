5990e89b614fc617c621dfbe46e21a1d296b3f63
IsmaelAlomari <ismaelalomari994@gmail.com>
2021-06-15 11:30:29 +0300

    express

---
+++ app.js
@@ -0,0 +1,12 @@
+const products = require("../data")
+const express = require("express");
+
+const app = express();
+
+app.listen(8000, () => {
+    console.log("The application is running on localhost:8000");
+  });
+
+  app.get("/products", (req, res) => {
+    res.json({products});
+  });
